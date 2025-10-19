17768ab5241a5fa7370a52ccf4aeccdbd7edfe6c
Dharini <cdharini85@gmail.com>
2025-10-19 15:36:45 +0800

    Created feature.py function to add 2 numbers

---
+++ feature.py
@@ -0,0 +1,14 @@
+def add_num(a,b):
+    """Returns the sum of two numbers.
+    
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+        
+    Returns:
+        int or float: The sum of the two numbers.
+        """
+    return a + b
+
+    sum_int = add_num(3, 5)
+    print(f"Sum of integers: {sum_int}")  # Output: Sum of integers: 8
